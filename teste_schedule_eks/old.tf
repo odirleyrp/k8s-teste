f8740e981423bfc8ac744bfc4b20088c2815b589
odirley <odirleyrp@gmail.com>
2023-01-17 21:49:59 -0300

    ajuste

---
+++ teste_schedule_eks/old.tf
@@ -0,0 +1,86 @@
+module "eks" {
+  source       = "terraform-aws-modules/eks/aws"
+  cluster_name = local.cluster_name
+  subnets      = module.vpc.private_subnets
+
+  tags = {
+    Environment = "training"
+    GithubRepo  = "terraform-aws-eks"
+    GithubOrg   = "terraform-aws-modules"
+  }
+
+  vpc_id = module.vpc.vpc_id
+  enable_irsa = true
+
+  worker_groups_launch_template = [
+    {
+      name                    = "spot-${var.cluster_name}"
+      override_instance_types = var.instance_types
+      spot_instance_pools     = 4
+      asg_max_size            = 5
+      asg_desired_capacity    = var.capacity
+      kubelet_extra_args      = "--node-labels=node.kubernetes.io/lifecycle=spot"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+      public_ip               = false 
+    },
+  ]
+
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+-----
+
+
+resource "aws_security_group" "worker_group_mgmt_one" {
+  name_prefix = "worker_group_mgmt_one"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+    ]
+  }
+}
+
+resource "aws_security_group" "worker_group_mgmt_two" {
+  name_prefix = "worker_group_mgmt_two"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "192.168.0.0/16",
+    ]
+  }
+}
+
+resource "aws_security_group" "all_worker_mgmt" {
+  name_prefix = "all_worker_management"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16",
+    ]
+  }
+}
